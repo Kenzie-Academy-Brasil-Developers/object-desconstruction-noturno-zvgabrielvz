874d0fc7939870854c5e60bc799fef911f4e6eb4
Gabriel Silva <gabriel_santos2006@hotmail.com>
2022-02-18 23:29:27 -0300

    finished

--- script.js
+++ script.js
@@ -28,3 +28,83 @@ const itemArray = [
         "section": "Hortifruti"
     },
 ]
+
+
+const testMap = (array) => {
+
+    const result = array.map((value) => {
+        let { id, price } = value
+        if (id > 2) { 
+
+            value.price = 2
+        
+        }
+        return {...value, price}
+    })
+    return result 
+}
+
+const testFilter = (array) => {
+    
+    const result = array.filter( ({id}) => {
+        return id <= 2
+    })
+    return result
+}
+
+const testFind = (array) => {
+
+    const result = array.find( ({id, product}) => {
+        return product === 'Leite'
+    })
+
+    return result;
+
+}
+
+const testReduce = (array) => {
+
+    const totalList = array.reduce((acc, {quantity, price}) => {
+        acc = acc + (quantity * price);
+
+        return acc
+    }, 0)
+
+    return totalList
+}
+
+const priceMaxthree = (array) => {
+    
+    const result = array.filter( ({price}) => {
+        return price > 3;
+    })
+    return result
+}
+
+
+const addNewPrice = (array) => {
+
+    const result = array.map((value) => {
+        let { product, price } = value
+        if (product === "Leite") { 
+
+            price = 4
+        
+        }
+        return {...value, price}
+    })
+    return result 
+}
+
+
+const costAllPadaria = (array) => {
+
+    const totalList = array.reduce((acc, {quantity, price, section}) => {
+        if(section === "Padaria"){
+            acc = acc + (quantity * price);
+        }
+        return acc
+    }, 0)
+
+    return totalList
+}
